cd0a6dc943cbf010a89efd7c6950ba13e7959d00
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2024-03-04 22:04:42 +0100

    lagt till javascript som kan markera på kartan beroende på vart användaren trycker

--- src/js/maps.js
+++ src/js/maps.js
@@ -10,17 +10,26 @@ const data_lank = "https://nominatim.openstreetmap.org/search?q";
 
 let dest_seach = document.getElementById('sok_destination');
 dest_seach.addEventListener('click', search_dest);
-
+let map;
+let marker;
 
 
 async function init_map()
 {
     try
     {
-        let url_sok = "https://nominatim.openstreetmap.org/search?q="+"Ludvika"+"&format=geojson";
+        let url_sok = "https://nominatim.openstreetmap.org/search?q="+"sweden"+"&format=geojson";
         let gps_kord= await get_data(url_sok);
-        drawmap([gps_kord.features[0].geometry.coordinates[1] ,gps_kord.features[0].geometry.coordinates[0]],"Ludvika");
-        
+        drawmap([gps_kord.features[0].geometry.coordinates[1] ,gps_kord.features[0].geometry.coordinates[0]],"sweden",4);
+        //Lägger till ett event för att kunna fånga upp kordinater från användares intergation. dvs att en användaresklick väljer område
+        map.on('click', function(e) {
+            var clickkordinates = map.mouseEventToLatLng(e.originalEvent); 
+            map.removeLayer(marker);
+            marker = L.marker([clickkordinates.lat ,clickkordinates.lng]).addTo(map)
+            .bindPopup("test")
+            .openPopup();
+        });
+
     }
     //Fångar upp eventuella fel som kan uppstå vid hämtning av data. 
     catch(error)
@@ -43,7 +52,7 @@ async function search_dest()
 
         let gps_kord= await get_data(url_sok);
 
-        drawmap([gps_kord.features[0].geometry.coordinates[1] ,gps_kord.features[0].geometry.coordinates[0]],inputdata);
+        drawmap([gps_kord.features[0].geometry.coordinates[1] ,gps_kord.features[0].geometry.coordinates[0]],inputdata,13);
 
         console.log("dest",url_sok);
     }
@@ -58,19 +67,21 @@ async function search_dest()
 
 
 //Ritar karta via chart.js
-function drawmap(kordinater,sokdata)
+function drawmap(kordinater,sokdata,zoomlevel)
 {
   
-    let map = L.DomUtil.get('map');
+    map = L.DomUtil.get('map');
     if(map != null){
         map._leaflet_id = null;
     }
 
-    map = L.map('map').setView(kordinater, 13);
+    map = L.map('map').setView(kordinater, zoomlevel);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-    L.marker(kordinater).addTo(map)
+    marker = L.marker(kordinater).addTo(map)
         .bindPopup(sokdata)
         .openPopup();
+
+        
 }
 
 //hämtar data asynkront baserat på input url
@@ -80,3 +91,4 @@ async function get_data(url_IN)
         const data = await response.json();
         return data;
 }
+
